77826df6fcc983f1371e2e5355b95c69dbe42390
isaacfrumkin <ifrumki1@jhu.edu>
2021-04-14 00:28:55 -0400

    Filtering with different paramters selected

--- ballotFiles/ballotFiles.js
+++ ballotFiles/ballotFiles.js
@@ -9,7 +9,7 @@ function ajaxRequest(params) {
 function dropdownToggle() {
   // select the main dropdown button element
   var dropdown = $(this).parent().parent().prev();
-  console.log(dropdown.val())
+  console.log(dropdown.text())
 
   // change the CONTENT of the button based on the content of selected option
   dropdown.html($(this).html());
@@ -25,10 +25,17 @@ function dropdownToggle() {
 
 $(document).ready(function () {
 
-  $("#table").hide();
+  $("#table tbody").hide();
   $("#downloadBtn").hide();
 
   $('.dropdown-menu a').on('click', dropdownToggle);
+
+//   $('#dropdownMenuState a').on('click', function() {
+//     console.log("changed");
+//     console.log($(this).text());
+//     $("#dropdownMenuElection").load("textdata/" + $(this).text() + ".txt");
+//     console.log($("#dropdownMenuElection").text());
+//  });
 
   var state_val = "";
   var election_val = "";
@@ -37,29 +44,45 @@ $(document).ready(function () {
   var status_val = "";
   var issue_val = "";
 
-
+  // Enter Button
   $("#enterBtn").on("click", function () {
     state_val = $("#dropdownState").text();
-    console.log($('#dropdownState').html());
+    console.log("HI");
+    console.log($("#dropdownElection").val());
+    console.log($("#dropdownState").attr('value'));
     election_val = $("#dropdownElection").text();
-    county_val = $("#dropdownCounty").text();
-    city_val = $("#dropdownCity").text();
-    status_val = $("#dropdownStatus").text();
-    issue_val = $("#dropdownIssue").text();
+    console.log($("#dropdownElection").attr('value'));
+    if (($("#dropdownCounty").text()).indexOf("County") < 0) {
+      console.log("here");
+      county_val = $("#dropdownCounty").text();
+    }
+    if (($("#dropdownCity").text()).indexOf("City") < 0) {
+      city_val = $("#dropdownCity").text();
+    }
+    if (($("#dropdownStatus").text()).indexOf("Status") < 0) {
+      status_val = $("#dropdownStatus").text();
+    } else {
+      // Default is to populate list with Rejected ballots
+      status_val = 'R';
+    }
+    if (($("#dropdownIssue").text()).indexOf("Issue") < 0) {
+      issue_val = $("#dropdownIssue").text();
+    }
+
     console.log(state_val);
     console.log(election_val);
-    // console.log(county_val);
-    // console.log(city_val);
-    // console.log(status_val);
-    // console.log(issue_val);
+    console.log(county_val);
+    console.log(city_val);
+    //console.log(status_val);
+    //console.log(issue_val);
 
     // , county: county_val, city: city_val, ballot_rtn_status: status_val, ballot_issue: issue_val
-    $("#table").show();
+    $("#table tbody").show();
     $.ajax({
       type: 'GET',
       url: 'http://128.220.221.36:5500/api/v1/ballots/',
       dataType: 'json',
-      data: { state: state_val, election_dt: election_val },
+      data: { state: state_val, election_dt: election_val, county: county_val, city: city_val, ballot_rtn_status: status_val, ballot_issue: issue_val },
       success: function (response) {
         console.log(response);
         $.each(response, function (i, item) {
@@ -83,16 +106,48 @@ $(document).ready(function () {
 
   });
 
+  //console.log("CHANGED");
+
+  // $("#dropdownState dropdownMenuState").change(function() {
+  //   console.log("change");
+  //   var $dropdown = $(this);
+  
+  //   $.getJSON("jsondata/data.json", function(data) {
+  //     var key = $dropdown.val();
+  //     var vals = [];
+                
+  //     switch(key) {
+  //       case 'GA':
+  //         vals = data.GA.split(",");
+  //         break;
+  //       case 'NC':
+  //         vals = data.NC.split(",");
+  //         break;
+  //       // case 'base':
+  //       //   vals = ['Please choose from above'];
+  //     }
+      
+  //     var $secondChoice = $("#dropdownElection");
+  //     $secondChoice.empty();
+  //     $.each(vals, function(index, value) {
+  //       $secondChoice.append("<li>" + value + "</li>");
+  //     });
+  
+  //   });
+  // });
 
 
+  // Reset Filters Button
   $("#clearBtn").on("click", function () {
-    $('#dropdownState').val(0);
     $('#dropdownState').html('Select State');
-    $('#dropdownElection').val(0);
     $('#dropdownElection').html('Select Election');
+    $('#dropdownCounty').html('County');
+    $('#dropdownCity').html('City');
+    $('#dropdownStatus').html('Status');
+    $('#dropdownIssue').html('Issue');
     $("#downloadBtn").hide();
     $("#table tbody").empty();
-    $("#table").hide();
+    $("#table tbody").hide();
 
   });
 
