060c008612af624700c5575d87d0b29b48a873e6
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2021-04-22 22:45:09 -0400

    Move button interaction to main.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,48 +1,6 @@
-// main file for user actions
-
-
-elections = {
-  "NC General Election (11/3/20)" : {
-    "state": "nc",
-    "election_dt": "11-03-2020"
-  },
-  "GA Senate Runoff (1/5/21)" : {
-    "state": "ga",
-    "election_dt": "01-04-2021"
-  }
-}
-
-// Default
-$(document).ready(function () {
-  render_election_data("ga", "01-04-2021");
-});
-
-function pressButton() {
-  // clear contents of old stuff
-  $("#quick_stats").empty();
-  $("#stats").empty();
-  $("#donut").empty();
-  $("#line").empty();
-
-  const e = document.getElementById("election-btn");
-  const title = document.getElementById('election-title')
-  const header = document.createElement('h2')
-  header.setAttribute('class', 'h2')
-  header.setAttribute('id', 'election')
-  header.textContent = e.value
-  title.replaceChild(header, document.getElementById('election'))
-
-  render_election_data(elections[e.value].state, elections[e.value].election_dt)
-}
-
+// Contains helper file for generating dashboard data
 
 function render_election_data(state, election_dt) {
-  // const e = document.getElementById("election-btn");
-  // const title = document.getElementById('election-title')
-  // const header = document.createElement('h2')
-  // header.setAttribute('class', 'h2')
-  // header.setAttribute('id', 'election')
-  // header.textContent = e.value
   const app = document.getElementById('stats')
   const container = document.createElement('div')
   container.setAttribute('class', 'flex_container')
