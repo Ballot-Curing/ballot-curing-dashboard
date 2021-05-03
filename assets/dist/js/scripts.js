5b46bb1725680812e50d50ebd3de0230d7b72eee
isaacfrumkin <ifrumki1@jhu.edu>
2021-05-02 20:06:44 -0400

    components

--- assets/dist/js/scripts.js
+++ assets/dist/js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     // all custom jQuery will go here
 
-    $("#nav-placeholder").load("/components/nav.html");
+    $("#nav-placeholder").load("../components/nav.html");
 
-    $("#header-placeholder").load("/components/header.html");
+    $("#header-placeholder").load("../components/header.html");
 
-    $("#footer-placeholder").load("/components/footer.html");
+    $("#footer-placeholder").load("../components/footer.html");
 
 
     $("#parameterDropdowns-placeholder").load("../components/parameterDropdowns");
